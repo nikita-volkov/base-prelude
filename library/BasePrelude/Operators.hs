8bcfd91dee285833b5d4f74e5109ad0255f786f4
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-02-15 17:37:43 +0300

    Add contravariant operators

--- library/BasePrelude/Operators.hs
+++ library/BasePrelude/Operators.hs
@@ -36,6 +36,12 @@ module BasePrelude.Operators
     (Data.Functor.<$>),
     (Data.Functor.<&>),
 
+    -- * From "Data.Functor.Contravariant"
+    (Data.Functor.Contravariant.>$),
+    (Data.Functor.Contravariant.>$<),
+    (Data.Functor.Contravariant.>$$<),
+    (Data.Functor.Contravariant.$<),
+
     -- * From "Data.Ord"
     (Data.Ord.<),
     (Data.Ord.<=),
@@ -66,6 +72,7 @@ import qualified Data.Bool
 import qualified Data.Eq
 import qualified Data.Function
 import qualified Data.Functor
+import qualified Data.Functor.Contravariant
 import qualified Data.Ord
 import qualified Data.Ratio
 import qualified Data.Semigroup
